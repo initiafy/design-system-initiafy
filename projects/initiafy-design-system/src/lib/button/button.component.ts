c612bc39291c9ca8fc0ebb519aabeedf5f578033
Fernanda Pieretti <fpieretti@initiafy.com>
2019-03-04 16:25:03 +0000

    Updating CSS according current project Testing use of simple button

--- projects/initiafy-design-system/src/lib/button/button.component.ts
+++ projects/initiafy-design-system/src/lib/button/button.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { text } from '@angular/core/src/render3';
 
 @Component({
-  selector: 'lib-button',
+  selector: 'initiafy-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  
+  @Input() type: string;
+  @Input() color: string;
+  @Input() text: string;
+  @Input() icon: string;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  handleButtonClick() {
-    console.log('clicked');
   }
 
 }
