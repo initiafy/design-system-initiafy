f6586c52098c5a2d9784cb54f09e0c028e07570a
Fernanda Pieretti <fpieretti@initiafy.com>
2019-03-04 09:54:22 +0000

    Updating packages to remove vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var concat = require('gulp-concat')
-var minify = require('gulp-minify-css')
 
 gulp.task('default', gulp.series((done) => {
     gulp.src([
@@ -11,8 +10,7 @@ gulp.task('default', gulp.series((done) => {
         "projects/initiafy-design-system/src/lib/styles/components/*.scss",
         "projects/initiafy-design-system/src/lib/styles/help/*.scss"
     ])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minify())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('src/assets/styles/'));
     done();
