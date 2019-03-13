d2990ed197c92f0c9a135083c83ae7bd0d10ab99
goughjo02 <goughjo02@gmail.com>
2019-03-13 16:34:01 +0000

    use modules for button and icon module

--- projects/design-system-initiafy/src/design-system-initiafy.module.ts
+++ projects/design-system-initiafy/src/design-system-initiafy.module.ts
@@ -3,22 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { ButtonComponent } from './lib/button/button.component';
 import { IconComponent } from './lib/icon/icon.component';
+import { InitiafyButtonModule } from './lib/button/buttons.module';
+import { InitiafyIconModule } from './lib/icon/icon.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatButtonModule,
-    MatTooltipModule
+    InitiafyButtonModule,
+    InitiafyIconModule
   ],
-  declarations: [
-    ButtonComponent,
-    IconComponent
-  ],
-  exports: [
-    ButtonComponent,
-    IconComponent
-  ]
+  exports: [InitiafyButtonModule, InitiafyIconModule]
 })
-export class DesignSystemInitiafyModule { }
+export class DesignSystemInitiafyModule {}
