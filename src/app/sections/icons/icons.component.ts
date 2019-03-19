6faaf7fe4188815e61f5587c78186536cc952a28
Fernanda Pieretti <fpieretti@initiafy.com>
2019-03-19 17:11:12 +0000

    Adjustments to interface

--- src/app/sections/icons/icons.component.ts
+++ src/app/sections/icons/icons.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonService } from 'src/app/common.service';
 
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
-  styleUrls: ['./icons.component.scss']
+  styleUrls: ['./icons.component.scss'],
+  providers: [CommonService]
 })
-export class IconsComponent implements OnInit {
+export class IconsComponent {
   public title = 'icon.html';
   public componentName = 'IconComponent';
   public module = 'InitiafyIconModule';
-  constructor() { }
+  public showCode = false;
 
-  ngOnInit() {
+  constructor(private common: CommonService) {}
+
+  copyToClipboard(item: string): void {
+    this.common.copyToClipboard(item);
   }
 
 }
