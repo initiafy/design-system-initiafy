e2aaf385f2af02fee2416206181ec7b0ebbbaa69
Fernanda Pieretti <fpieretti@initiafy.com>
2019-03-26 16:28:53 +0000

    update_design_system Initiafy Cards Code Mirror height

--- projects/design-system-initiafy/src/lib/card/card.component.ts
+++ projects/design-system-initiafy/src/lib/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'initiafy-card',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent  {
+  @Input() size: 'small' | 'standard' | 'medium' | 'big' | 'bigger'= 'standard';
 
   constructor() { }
 
