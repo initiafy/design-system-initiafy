bb1e91f3ece128717b6ecf879bb96bae8c1698fd
Fernanda Pieretti <fpieretti@initiafy.com>
2019-03-27 12:59:07 +0000

    New font files Remove unnecessary background color New class button wrapper

--- projects/design-system-initiafy/gulpfile.js
+++ projects/design-system-initiafy/gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var concat = require('gulp-concat')
+var autoprefixer = require('gulp-autoprefixer')
 
+/*
+ * CSS gulp function
+ * Select all css files of this lib
+ * Convert files from sass to css and compress them
+ * Set autoprefixer on styles
+ * Concatenate files into one min.css
+ * Save minified file into dist folder
+ */
 gulp.task('css', gulp.series((done) => {
     gulp.src([
         "src/lib/styles/imports/fonts.scss",
@@ -15,15 +24,30 @@ gulp.task('css', gulp.series((done) => {
         "src/lib/styles/help/*.scss"
     ])
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('./dist/lib/styles/'));
     done();
 }));
 
+/*
+ * COPY gulp function
+ * Select all font files of this lib
+ * Save into dist folder
+ */
 gulp.task('copy', gulp.series((done) => {
-    gulp.src("src/lib/styles/assets/fonts/*")
+    gulp.src([
+        "src/lib/styles/imports/initiafyfont.eot",
+        "src/lib/styles/imports/initiafyfont.svg",
+        "src/lib/styles/imports/initiafyfont.ttf",
+        "src/lib/styles/imports/initiafyfont.woff"
+    ])
     .pipe(gulp.dest('./dist/lib/styles/'));
     done();
 }));
 
+/*
+ * Default task 
+ * Run into npm run build script
+ */
 gulp.task('default', gulp.series('css', 'copy'));
