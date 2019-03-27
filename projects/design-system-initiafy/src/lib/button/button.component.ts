fefff066e59eb29d15f5049c3811288b9a9c2be2
goughjo02 <goughjo02@gmail.com>
2019-03-27 16:47:01 +0000

    add white button & bump version

--- projects/design-system-initiafy/src/lib/button/button.component.ts
+++ projects/design-system-initiafy/src/lib/button/button.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'initiafy-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {
-  @Input() design: 'main' | 'stroke' | 'basic' = 'main';
+export class ButtonComponent implements OnInit {
+  @Input() design: 'main' | 'stroke' | 'basic' | 'white' = 'main';
   @Input() color: 'primary' | 'secondary';
   @Input() label: string;
   @Input() type: 'submit' | 'reset' | 'button' = 'submit';
   @Input() icon: string;
-  @Input() iconFont: 'glyphicon' | 'font-awesome-brand' | 'font-awesome-solid' | 'material-icons' = 'material-icons';
+  @Input() iconFont:
+    | 'glyphicon'
+    | 'font-awesome-brand'
+    | 'font-awesome-solid'
+    | 'material-icons' = 'material-icons';
   @Input() tooltip: string;
   @Input() disabled = false;
   @Input() buttonClasses = '';
@@ -19,4 +23,9 @@ export class ButtonComponent {
 
   constructor() {}
 
+  ngOnInit() {
+    if (this.design === 'white') {
+      this.color = null;
+    }
+  }
 }
