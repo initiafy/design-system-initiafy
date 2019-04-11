6666cc3bf64463b40a4d3c9f7375e47919db0478
goughjo02 <goughjo02@gmail.com>
2019-04-11 13:39:41 +0100

    add descriptions to button properties

--- projects/design-system-initiafy/src/lib/button/button.component.ts
+++ projects/design-system-initiafy/src/lib/button/button.component.ts
@@ -6,15 +6,25 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  /** Design of the button */
   @Input() design: 'main' | 'stroke' | 'basic' | 'white' = 'main';
+  /** Color palette of the button */
   @Input() color: 'primary' | 'secondary';
+  /** Text label for the button  */
   @Input() label: string;
+  /** Type attribute of the button */
   @Input() type: 'submit' | 'reset' | 'button' = 'submit';
+  /** Icon name of icon to appear */
   @Input() icon: string;
+  /** Icon library to whivh the icon belongs */
   @Input() iconFont: 'initiafy-icons' | 'glyphicon' | 'font-awesome-brand' | 'font-awesome-solid' | 'material-icons' = 'material-icons';
+  /** Additional css classes to apply to the button */
   @Input() buttonClasses: string = '';
+  /** Text for the button's tooltip */
   @Input() tooltip: string;
+  /** Disable state of the button */
   @Input() disabled = false;
+  /** Method called when button is clicked */
   @Output() callback = new EventEmitter();
 
   private _classes: string;
