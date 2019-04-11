8cd5f80b2924c4346059bfbab8881085f6400474
goughjo02 <goughjo02@gmail.com>
2019-04-11 12:11:29 +0100

    small fix for scoll on nav behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DocumentationService } from './core/documentation/documentation.service';
 import { ContentComponent } from './shared';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,27 @@ import { ContentComponent } from './shared';
 export class AppComponent implements OnInit {
   title = 'InitiafyDesignSystem';
   @ViewChild('content') content: ContentComponent;
-  constructor(private startup: DocumentationService) {}
+  constructor(private startup: DocumentationService, private router: Router) {}
   ngOnInit() {
     // If there is no startup data received (maybe an error!)
     // navigate to error route
     if (!this.startup.startupData) {
       console.error('no startupdata');
     }
+    // This is for the sidenav content - to scroll to top when navigation occurs
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      const scrollToTop = window.setInterval(function () {
+          const pos = window.pageYOffset;
+          if (pos > 0) {
+              window.scrollTo(0, pos - 20); // how far to scroll on each step
+          } else {
+              window.clearInterval(scrollToTop);
+          }
+      }, 16); // how fast to scroll (this equals roughly 60 fps)
+  });
   }
 
   toggleNav() {
