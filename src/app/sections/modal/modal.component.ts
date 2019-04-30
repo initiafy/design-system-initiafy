4fd831aae3f4ccbfa18dceafbfd4c72faf2275e0
JGough-Initiafy <jgough@initiafy.com>
2019-04-30 14:37:33 +0100

    add working example of modal working

--- src/app/sections/modal/modal.component.ts
+++ src/app/sections/modal/modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, InjectionToken, Injector } from '@angular/core';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
+import { ExampleModalComponent } from './example-modal/example-modal.component';
+import { CONTAINER_DATA, InjectionData } from './constants';
 
 @Component({
   selector: 'app-modal',
@@ -8,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
 export class ModalDocumentationComponent implements OnInit {
   public componentName = 'ModalComponent';
   public module = 'InitiafyModalModule';
+  public injectionData: InjectionData;
 
-  constructor() { }
+  constructor(private _overlay: Overlay, private _injector: Injector) { }
 
   ngOnInit() {
+  }
+
+  public handleButtonClick(): void {
+    const overlayRef = this._overlay.create();
+    this.injectionData = {
+      closeModal: () => overlayRef.detach()
+    }
+    const userProfilePortal = new ComponentPortal(ExampleModalComponent, null, this.createInjector(this.injectionData));
+    overlayRef.attach(userProfilePortal);
+  }
+
+  private createInjector(dataToPass): PortalInjector {
+    const injectorTokens = new WeakMap();
+    injectorTokens.set(CONTAINER_DATA, dataToPass);
+    return new PortalInjector(this._injector, injectorTokens);
   }
 
 }
--- src/app/sections/modal/modal.module.ts
+++ src/app/sections/modal/modal.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverlayModule, FullscreenOverlayContainer, OverlayContainer } from '@angular/cdk/overlay';
 
 import { ModalRoutingModule } from './modal-routing.module';
 import { ModalDocumentationComponent } from './modal.component';
 import { InitiafyModalModule } from 'projects/design-system-initiafy/src/public_api';
 import { SharedModule } from 'src/app/shared/shared.modules';
+import { ExampleModalComponent } from './example-modal/example-modal.component';
 
 @NgModule({
-  declarations: [ModalDocumentationComponent],
+  declarations: [ModalDocumentationComponent, ExampleModalComponent],
   imports: [
     CommonModule,
     SharedModule,
+    OverlayModule,
     ModalRoutingModule,
     InitiafyModalModule
-  ]
+  ],
+  providers: [{provide: OverlayContainer, useClass: FullscreenOverlayContainer}],
+  entryComponents: [ExampleModalComponent]
 })
 export class ModalModule { }
