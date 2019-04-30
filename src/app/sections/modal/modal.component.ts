441568a83cb745fbf42a1f1b4e6e3a8ae09a4c61
JGough-Initiafy <jgough@initiafy.com>
2019-04-30 14:13:44 +0100

    add documentation component to modal section

--- src/app/sections/modal/modal.component.ts
+++ src/app/sections/modal/modal.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalDocumentationComponent implements OnInit {
+  public componentName = 'ModalComponent';
+  public module = 'InitiafyModalModule';
 
   constructor() { }
 
--- src/app/sections/modal/modal.module.ts
+++ src/app/sections/modal/modal.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ModalRoutingModule } from './modal-routing.module';
 import { ModalDocumentationComponent } from './modal.component';
+import { InitiafyModalModule } from 'projects/design-system-initiafy/src/public_api';
+import { SharedModule } from 'src/app/shared/shared.modules';
 
 @NgModule({
   declarations: [ModalDocumentationComponent],
   imports: [
     CommonModule,
-    ModalRoutingModule
+    SharedModule,
+    ModalRoutingModule,
+    InitiafyModalModule
   ]
 })
 export class ModalModule { }
