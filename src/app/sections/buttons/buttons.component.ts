ec5b4aece202f8ed79c1e9f2ce4b63000194bb5b
JGough-Initiafy <jgough@initiafy.com>
2019-05-01 17:04:17 +0100

    update styl;e of button section

--- src/app/sections/buttons/buttons.component.ts
+++ src/app/sections/buttons/buttons.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ButtonsService } from '../../services/buttons.service';
 import { Buttons } from 'src/app/models/buttons';
+import { MatSlideToggleChange } from '@angular/material';
 
 @Component({
   selector: 'app-buttons',
@@ -12,7 +13,7 @@ export class ButtonsComponent implements OnInit {
   public componentName = 'ButtonComponent';
   public module = 'InitiafyButtonModule';
   public buttons: Array<Array<Buttons>> = [];
-
+  public darkBackground: boolean;
   constructor(
     private buttonsService: ButtonsService
   ) {}
@@ -24,6 +25,10 @@ export class ButtonsComponent implements OnInit {
     this.buttons.push(this.buttonsService.getButtons('stroke', 'secondary', 'Stroke'));
     this.buttons.push(this.buttonsService.getButtons('white', '', 'White'));
     this.buttons.push(this.buttonsService.getButtons('basic', '', 'Basic'));
+  }
+
+  public handleSlideChange(event: MatSlideToggleChange): void {
+    this.darkBackground = event.checked;
   }
 
 }
