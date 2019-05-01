be52964bbeaf32bb39e79909578b6aa618f95929
Fernanda Pieretti <fpieretti@initiafy.com>
2019-05-01 12:12:39 +0100

    Add determinate mode to progress bar and loading card

--- projects/design-system-initiafy/src/lib/progress-bar/progress-bar.component.ts
+++ projects/design-system-initiafy/src/lib/progress-bar/progress-bar.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProgressBarComponent implements OnInit {
   @Input() isLoading = '';
+  @Input() mode: 'indeterminate' | 'determinate' = 'indeterminate';
+  @Input() value: number = null;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.width);
+  }
+
+  get width() {
+    if (this.value) {
+      return this.value + '%';
+    }
   }
 
 }
